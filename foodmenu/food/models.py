87b2ae7154bd6284de99f2bbe4ec7f97fb798159
SABBIR HOSSAIN <sabbirhossain@SABBIRs-Laptop.local>
2024-05-13 13:00:41 +0900

    initial

--- foodmenu/food/models.py
+++ foodmenu/food/models.py
@@ -8,3 +8,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_description = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg")

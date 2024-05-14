76e627f84aba3897be47966c4053e73a1e162637
SABBIR HOSSAIN <sabbirhossain@SABBIRs-Laptop.local>
2024-05-14 19:41:22 +0900

    dsd

--- foodmenu/food/urls.py
+++ foodmenu/food/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:item_id>/', views.details, name='details'),
     path('item/', views.item, name='item'),
+    #add
+    path('add', views.create_item, name='create_item'),
+    #edit
+    path('update/<int:id>/', views.update_item, name='update_item'),
+    path('delete/<int:id>/', views.delete_item, name='delete_item'),
 ]

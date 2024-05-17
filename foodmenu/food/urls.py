de4715b1705ff72435d00ccfc48ef6fe04bdd04f
SABBIR HOSSAIN <sabbirhossain@SABBIRs-Laptop.local>
2024-05-17 13:19:24 +0900

    ds

--- foodmenu/food/urls.py
+++ foodmenu/food/urls.py
@@ -4,10 +4,12 @@ from django.urls import path
 app_name = 'food'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:item_id>/', views.details, name='details'),
+    #path('<int:item_id>/', views.details, name='details'),
+    path('<int:pk>/',views.FoodDetail.as_view(),name='details'),
     path('item/', views.item, name='item'),
     #add
-    path('add', views.create_item, name='create_item'),
+    path('add',views.CreateItem.as_view(),name='create_item'),
+    #path('add', views.create_item, name='create_item'),
     #edit
     path('update/<int:id>/', views.update_item, name='update_item'),
     path('delete/<int:id>/', views.delete_item, name='delete_item'),
--- foodmenu/food/views.py
+++ foodmenu/food/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from .models import Item
 from django.template import loader
 from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 def index(request):
@@ -13,6 +16,12 @@ def index(request):
     }
     return render(request, 'food/index.html', context)
     #return HttpResponse(template.render(context, request))
+
+#Another Way Class based view
+class IndexClassView(ListView):
+   model = Item
+   templateName = 'food/index.html'
+   context_object_name = 'item_name'
 
 
 def item(request):
@@ -26,6 +35,16 @@ def details(request, item_id):
     return render(request, 'food/detail.html', context)
 
     #return HttpResponse('this is item id: %s' %item_id)
+#class Based
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+
+
+
+class foodDetail(DetailView):
+    model = item
+    template_name = 'food/detail.html'
 
 def create_item(request):
     # print(request)
@@ -37,6 +56,22 @@ def create_item(request):
         return redirect('food:index')
     
     return render(request, 'food/item-from.html', {'form' : form})
+
+#Classbased View
+# this is a class based view for create item
+ 
+class CreateItem(CreateView):
+    model = Item;
+    fields = ['item_name','item_description','item_price','item_image']
+    template_name='food/item-from.html'
+ 
+    def form_valid(self,form):
+        form.instance.user_name = self.request.user
+ 
+        return super().form_valid(form)
+
+
+
 
 
 def update_item(request, id):
